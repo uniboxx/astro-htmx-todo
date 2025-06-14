4fc0d7a24181dc737053264c47023f98e7da4f40
unibox <unibox@duck.com>
2025-06-14 09:35:15 +0200

    final touches 2

--- src/pages/api/message.ts
+++
@@ -1,9 +0,0 @@
-import type { APIContext } from 'astro';
-
-export async function GET(ctx: APIContext) {
-  return new Response('HTMX working', {
-    headers: {
-      'Last-Modified': '11 June 2025',
-    },
-  });
-}
